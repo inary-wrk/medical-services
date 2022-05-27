e1161eb1eedacf79c6419b7a38ca6b01afa259a1
Semyon <inary.wrk@gmail.com>
2022-05-27 17:13:08 +0300

    one common repository for doctor

---
+++ src/datalayer.abstraction/Repositories/IDoctorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using datalayer.abstraction.Entities;
+
+namespace datalayer.abstraction.Repositories
+{
+    public interface IDoctorQueryRepository
+    {
+        public Task<Doctor> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+    }
+
+    public interface IDoctorCommandRepository
+    {
+        public Task<Doctor> AddAsync(Doctor doctor, CancellationToken cancellationToken = default);
+        public Task<Doctor> UpdateAsync(Doctor doctor, CancellationToken cancellationToken = default);
+        public Task DeleteAsync(long id, CancellationToken cancellationToken = default);
+    }
+}
