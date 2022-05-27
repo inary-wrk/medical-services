db2404cd947274a7cb5048d166d15d54ff1ffa6b
Semyon <inary.wrk@gmail.com>
2022-05-27 16:59:17 +0300

    Doctor Details, Create command and query classes and handlers has been added

---
+++ src/businesslogic/Features/DoctorFeatures/DoctorCreate.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using businesslogic.abstraction.Contracts;
+using businesslogic.abstraction.Dto;
+using businesslogic.abstraction.ValueObjects;
+using datalayer.abstraction.Entities;
+using datalayer.abstraction.Repositories;
+using MediatR;
+
+namespace businesslogic.Features.DoctorFeatures
+{
+    public static class DoctorCreate
+    {
+        public record Command(DoctorDto.Request.Create Doctor) : ICommandRequest<Id>;
+
+        public class Handler : IRequestHandler<Command, Id>
+        {
+            private readonly IDoctorCommandRepository _repository;
+            private readonly IMapper _mapper;
+
+            public Handler(IDoctorCommandRepository repository, IMapper mapper)
+            {
+                _repository = repository;
+                _mapper = mapper;
+            }
+
+            public async Task<Id> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var doctor = _mapper.Map<DoctorDto.Request.Create, Doctor>(request.Doctor);
+                var result = await _repository.AddAsync(doctor, cancellationToken);
+                return result.Id;
+            }
+        }
+    }
+}
---
+++ src/businesslogic/Features/DoctorFeatures/DoctorDetails.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using businesslogic.abstraction.ValueObjects;
+using businesslogic.abstraction.Dto;
+using MediatR;
+using System.Threading;
+using businesslogic.abstraction.Contracts;
+using datalayer.abstraction.Repositories;
+using datalayer.abstraction.Entities;
+
+namespace businesslogic.Features.DoctorFeatures
+{
+    public static class DoctorDetails
+    {
+        public record Query(Id DoctorId) : IQueryRequest<DoctorDto.Doctor>;
+
+        public class QueryHandler : IRequestHandler<Query, DoctorDto.Doctor>
+        {
+            private readonly IDoctorQueryRepository _repository;
+            private readonly IMapper _mapper;
+
+            public QueryHandler(IDoctorQueryRepository repository, IMapper mapper)
+            {
+                _repository = repository;
+                _mapper = mapper;
+            }
+
+            public async Task<DoctorDto.Doctor> Handle(Query request, CancellationToken cancellationToken)
+            {
+              var result = await _repository.GetByIdAsync(request.DoctorId, cancellationToken);
+                return _mapper.Map<Doctor, DoctorDto.Doctor>(result);
+            }
+        }
+    }
+}
