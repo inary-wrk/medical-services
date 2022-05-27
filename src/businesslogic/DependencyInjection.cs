3734ba3d76e9bc8472bab8ebe37fb2155be9d554
Semyon <inary.wrk@gmail.com>
2022-05-27 17:02:20 +0300

    Dependency injection extension methods for business logic using scrutor to scan handlers.

---
+++ src/businesslogic/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace businesslogic
+{
+    public static class DependencyInjection
+    {
+       public static IServiceCollection RegisterBusinesslogic(this IServiceCollection services)
+        {
+            services.Scan(scan => scan
+                .FromCallingAssembly()
+                .AddClasses(classes=> classes.AssignableTo(typeof(IRequestHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+            return services;
+        }
+    }
+}
