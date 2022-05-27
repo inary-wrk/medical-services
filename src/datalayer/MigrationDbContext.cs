77b9b4ac85cd55274739cf4d430c28148c696196
Semyon <inary.wrk@gmail.com>
2022-05-27 17:19:34 +0300

    has been added to fix migration db context issue

---
+++ src/datalayer/MigrationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace datalayer
+{
+    public sealed class MigrationDbContext : BaseDbContext
+    {
+        public MigrationDbContext(DbContextOptions<MigrationDbContext> contextOptions)
+            : base(contextOptions)
+        {
+        }
+    }
+}
